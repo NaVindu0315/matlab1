6c05ca071235530a205c12603e6db9d20bd51ed4
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-10-09 22:05:41 +0530

    calling functions

---
+++ secondfunctions.m
@@ -0,0 +1,10 @@
+col_row_equal = magic(4)
+max_firstcol = min(col_row_equal);
+min_firstcol = min(col_row_equal(:,1));
+min_firstrow = min(col_row_equal(1,:));
+mat_size = size(col_row_equal);
+
+vector=[2, 1.8, 1.5 ,1.9];
+variance = var(vector);
+standard_deviation = std(vector);
+
