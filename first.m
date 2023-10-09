7bff381f38bc8b31e49245b0ffdcd220814c3e7c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-10-09 21:35:57 +0530

    second

--- first.m
+++ first.m
@@ -2,10 +2,16 @@ clear all;
 %arrag creation
 %row matrix
 A = [2 100 4 11];
-b = [2,0,5,1];
+B = [2,0,5,1];
  %3*3 matrixx
 c = [4 3 1 ; 3 3 2 ; 2 3 5];
 ero = zeros(3,1)
 Ones = ones(3,2)
 fucks = ones(3,5)
+OP = A +3;
+OP
+OP1 = OP'
+Newarray = [A,B];
+col_row_equal = magic(4)
+
 
